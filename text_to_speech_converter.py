cbf9ad5c9467128123dda47d5e2f040a8be2a23e
Aidan Tiu <tiuprof77@gmail.com>
2024-03-24 11:26:57 +0800

    Import and Initialize text converter function

--- text_to_speech_converter.py
+++ text_to_speech_converter.py
@@ -1,3 +1,8 @@
 # Ask for user input
 # Convert the text to speech using the pyttsx3 init() function
 # Initialize functions to convert text to speech
+
+import pyttsx3
+
+# Initialize the text to speech converter function
+text_speech = pyttsx3.init()
